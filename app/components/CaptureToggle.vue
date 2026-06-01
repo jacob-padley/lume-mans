64428050ae96a7c0aa9e61e2327833ce10c740a5
Jacob Padley <jacob5180@hotmail.co.uk>
2026-06-01 17:45:55 +0100

    feat: start/stop capture button

---
+++ app/components/CaptureToggle.vue
@@ -0,0 +1,13 @@
+<template>
+  <UButton class="font-mono" :color="model ? 'error' : 'success'" @click="toggle"
+    >{{ model ? 'Stop' : 'Start' }} Watcher</UButton
+  >
+</template>
+
+<script setup lang="ts">
+const model = defineModel<boolean>({ default: false });
+
+function toggle() {
+  model.value = !model.value;
+}
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <UPageHeader title="Link Mans" />
     <TrackStatus class="mx-auto w-3/4 h-24 mb-3"> Track Clear </TrackStatus>
+    <CaptureToggle class="mx-auto block mt-10 mb-3" />
     <VideoInputMenu class="mx-auto block w-3/4" />
   </div>
 </template>
